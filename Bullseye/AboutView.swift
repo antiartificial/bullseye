52c7af507ad53ce167d37677036e563629515c3b
Aaron Barton <aaronb@tkmkt.com>
2020-07-27 22:46:37 -0700

    Bull's eye iOS app.

---
+++ Bullseye/AboutView.swift
@@ -0,0 +1,62 @@
+//
+//  AboutView.swift
+//  Bullseye
+//
+//  Created by Aaron Barton on 7/27/20.
+//  Copyright © 2020 Aaron Barton. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    
+    let beige = Color(red: 255.0 / 250.0 , green: 214.0 / 255.0, blue: 179.0 / 255.0)
+    
+    struct AboutHeadingStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                .padding(.top, 20)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    struct AboutBodyStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .padding(.leading, 60)
+                .padding(.trailing, 20)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    struct Shadow: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+        }
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(AboutBodyStyle())
+                Text("Your goal is to place the slider as close as possible to the target value. åThe closer you are, the more points you score.").modifier(AboutBodyStyle()).lineLimit(nil)
+                Text("Enjoy!").modifier(AboutBodyStyle())
+            }
+            .navigationBarTitle("About Bullseye")
+            .background(beige)
+        }
+        .background(Image("Background"))
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView().previewLayout(.fixed(width: 896, height: 414))
+    }
+}
